1109bef88aa4bc91ed8119db95acedfb93deaef0
Adrien Bardes <abardes@fb.com>
2022-04-22 06:02:24 -0700

    Fix submitit

--- run_with_submitit.py
+++ run_with_submitit.py
@@ -6,50 +6,31 @@
 
 
 import argparse
-import submitit
 import os
 import uuid
 from pathlib import Path
 
 import main_vicreg
+import submitit
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(
-        "Submitit for VICReg", parents=[main_vicreg.get_arguments()]
-    )
-    parser.add_argument(
-        "--nodes", default=4, type=int, help="Number of nodes to request"
-    )
-    parser.add_argument(
-        "--ngpus", default=8, type=int, help="Number of gpus to request on each node"
-    )
-    parser.add_argument(
-        "--timeout", default=72, type=int, help="Duration of the job, in hours"
-    )
+    parser = argparse.ArgumentParser("Submitit for VICReg", parents=[main_vicreg.get_arguments()])
+    parser.add_argument("--nodes", default=2, type=int, help="Number of nodes to request")
+    parser.add_argument("--ngpus", default=8, type=int, help="Number of gpus to request on each node")
+    parser.add_argument("--timeout", default=2800, type=int, help="Duration of the job")
     parser.add_argument("--job_name", default="vicreg", type=str, help="Job name")
-    parser.add_argument(
-        "--partition", default="mypartition", type=str, help="Partition where to submit"
-    )
-    parser.add_argument(
-        "--constraint",
-        default="",
-        type=str,
-        help="Slurm constraint. Use 'volta32gb' for Tesla V100 with 32GB",
-    )
-    parser.add_argument(
-        "--comment",
-        default="",
-        type=str,
-        help="Comment to pass to scheduler, e.g. priority message",
-    )
+    parser.add_argument("--partition", default="learnfair", type=str, help="Partition where to submit")
+    parser.add_argument("--use_volta32", action='store_true', help="Big models? Use this")
+    parser.add_argument('--comment', default="", type=str,
+                        help='Comment to pass to scheduler, e.g. priority message')
     return parser.parse_args()
 
 
 def get_shared_folder() -> Path:
     user = os.getenv("USER")
     if Path("/checkpoint/").is_dir():
-        p = Path(f"/checkpoint/{user}/vicreg")
+        p = Path(f"/checkpoint/{user}/vicreg/experiments")
         p.mkdir(exist_ok=True)
         return p
     raise RuntimeError("No shared folder available")
@@ -64,31 +45,77 @@ def get_init_file():
     return init_file
 
 
+class Trainer(object):
+    def __init__(self, args):
+        self.args = args
+
+    def __call__(self):
+        import main_vicreg
+
+        self._setup_gpu_args()
+        main_vicreg.main(self.args)
+
+    def checkpoint(self):
+        import submitit
+
+        self.args.dist_url = get_init_file().as_uri()
+        print("Requeuing ", self.args)
+        empty_trainer = type(self)(self.args)
+        return submitit.helpers.DelayedSubmission(empty_trainer)
+
+    def _setup_gpu_args(self):
+        import submitit
+        from pathlib import Path
+
+        job_env = submitit.JobEnvironment()
+        self.args.exp_dir = Path(str(self.args.exp_dir).replace("%j", str(job_env.job_id)))
+        self.args.gpu = job_env.local_rank
+        self.args.rank = job_env.global_rank
+        self.args.world_size = job_env.num_tasks
+        print(f"Process group: {job_env.num_tasks} tasks, rank: {job_env.global_rank}")
+
+
 def main():
     args = parse_args()
+    if args.exp_dir == "":
+        args.exp_dir = get_shared_folder() / "%j"
+    Path(args.exp_dir).mkdir(parents=True, exist_ok=True)
+    executor = submitit.AutoExecutor(folder=args.exp_dir, slurm_max_num_timeout=30)
 
-    args.exp_dir.mkdir(parents=True, exist_ok=True)
-    executor = submitit.AutoExecutor(folder=args.exp_dir)
+    num_gpus_per_node = args.ngpus
+    nodes = args.nodes
+    timeout_min = args.timeout
+
+    partition = args.partition
+    kwargs = {}
+    if args.use_volta32:
+        kwargs['slurm_constraint'] = 'volta32gb'
+    if args.comment:
+        kwargs['slurm_comment'] = args.comment
 
     executor.update_parameters(
         name=args.job_name,
-        mem_gb=40 * args.ngpus,
-        gpus_per_node=args.ngpus,
-        tasks_per_node=args.ngpus,
+        mem_gb=40 * num_gpus_per_node,
+        gpus_per_node=num_gpus_per_node,
+        tasks_per_node=num_gpus_per_node,  # one task per GPU
         cpus_per_task=10,
-        nodes=args.nodes,
-        timeout_min=args.timeout * 60,
-        slurm_partition=args.partition,
+        nodes=nodes,
+        timeout_min=timeout_min,  # max is 60 * 72
+        # Below are cluster dependent parameters
+        slurm_partition=partition,
         slurm_signal_delay_s=120,
-        slurm_constraint=args.constraint,
-        slurm_comment=args.comment,
+        **kwargs
     )
 
-    args.dist_url = get_init_file().as_uri()
-    print("Distributed URL: ", args.dist_url)
+    executor.update_parameters(name="vicreg")
 
-    job = executor.submit(main_vicreg.main, args)
-    print("Submitted job_id:", job.job_id)
+    args.dist_url = get_init_file().as_uri()
+
+    trainer = Trainer(args)
+    job = executor.submit(trainer)
+
+    print(f"Submitted job_id: {job.job_id}")
+    print(f"Logs and checkpoints will be saved at: {args.exp_dir}")
 
 
 if __name__ == "__main__":
