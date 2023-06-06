186c9a4eded7490b3e08e70e3e86fa7009d0b64c
Yufeng Luo <brookluo@thetalogin5.tmi.alcf.anl.gov>
2023-06-06 17:07:08 +0000

    Cropping rgb image to match thermal

--- sage_transform.py
+++ sage_transform.py
@@ -11,6 +11,11 @@ class SageTransform(object):
                 # these parameters are from augmentation in vicreg
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225]),
+                # crop the image to match the size of thermal image\
+                # keep the same aspect ratio 4/3
+                # TODO try to train the NN with different ratio
+                transforms.CenterCrop((1800, 2400)),
+                transforms.Resize((600, 800))
             ]
         )
         self.thermal_transform = transforms.Compose(
